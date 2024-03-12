403400d09de04466b809838db16b602bd6aca00c
imfarhat <ifarhat.dev@gmail.com>
2024-03-13 03:39:11 +0530

    instrcution message for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,35 +2,41 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <a
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="nav-link-n"
-        >
-          <img
-            src="https://raw.githubusercontent.com/imfarhat/AttendQR/main/public/apple-touch-icon.png"
-            alt="AttendQR Logo"
-            className="h-12 rounded shadow-sm"
-          />
-        </a>
+    <>
+      {" "}
+      <header>
+        <nav>
+          <a
+            href="/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-link-n"
+          >
+            <img
+              src="https://raw.githubusercontent.com/imfarhat/AttendQR/main/public/apple-touch-icon.png"
+              alt="AttendQR Logo"
+              className="h-12 rounded shadow-sm"
+            />
+          </a>
 
-        <a
-          href="https://farhateservices.pages.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="nav-link-n"
-        >
-          <img
-            src="https://farhateservices.pages.dev/logo-head-black.png"
-            alt="Farhat e Services & Assitance"
-            className="h-12 rounded shadow-sm bg-white"
-          />
-        </a>
-      </nav>
-    </header>
+          <a
+            href="https://farhateservices.pages.dev"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="nav-link-n"
+          >
+            <img
+              src="https://farhateservices.pages.dev/logo-head-black.png"
+              alt="Farhat e Services & Assitance"
+              className="h-12 rounded shadow-sm bg-white"
+            />
+          </a>
+        </nav>
+      </header>
+      <h1 className="p-2 text-xs md:text-base text-center">
+        Scan your college Id Card's QR or Barcode (Unversity Roll No.)
+      </h1>
+    </>
   );
 }
 
